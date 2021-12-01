a2358e876a738b1c2c03fa3c9d9a0a3df05a37b1
EduardoLazaroo <eduardolazaroso@hotmail.com>
2021-12-01 12:53:35 -0300

    suba

---
+++ Native_functions/aula01.php
@@ -0,0 +1,14 @@
+<?php
+    function mostrarEduardo(){
+        echo '<h2>Nome: Eduardo</h2>';
+    }
+    
+    mostrarEduardo();
+
+
+    function mostrarNome($nome){
+        echo '<h2>Nome: </h2>'.$nome;
+    }
+    
+    mostrarNome('nome');
+?>
